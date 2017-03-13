d4ee952e40160baa91ddf541051cbb9637f9cb38
vigorakbar <vigorakbar@gmail.com>
2017-03-13 20:07:13 +0700

    benerin format

--- habitat.cpp
+++ habitat.cpp
@@ -5,30 +5,30 @@
 #include "habitat.h"
 using namespace std;
 
-Habitat::Habitat(char s){
+Habitat::Habitat(char s) {
   symbol = s;
   initsymbol = s;
-  for (int i=0; i < 4; ++i){
+  for (int i = 0; i < 4; ++i) {
     sekat[i] = false;
   }
 }
 
-Habitat::~Habitat(){
+Habitat::~Habitat() {
   
 }
 
-void Habitat::ToggleSekat(int direction){
-  if (sekat[direction]){
+void Habitat::ToggleSekat(int direction) {
+  if (sekat[direction]) {
     sekat[direction] = false;
-  } else{
+  } else {
     sekat[direction] = true;
   }
 }
 
-bool Habitat::GetSekat(int direction) const{
+bool Habitat::GetSekat(int direction) const {
   return sekat[direction];
 }
 
-void Habitat::Interact() const{
+void Habitat::Interact() const {
   
 }
